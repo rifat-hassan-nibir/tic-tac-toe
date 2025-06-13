899fe0a58ed190fca147f83e684c0d95d038104b
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-06-13 07:47:47 +0600

    feat: add dynamic value and click function as prop

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,10 @@
-const Square = () => {
+const Square = ({ value, onSquareClick }) => {
   return (
-    <button className="h-[60px] w-[60px] bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
-      X
+    <button
+      onClick={onSquareClick}
+      className="h-[60px] w-[60px] bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+    >
+      {value}
     </button>
   );
 };
